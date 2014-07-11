24d2ec5689d7ba027963f5f2a15afb080bcc3a27
Eric Ito <eric.ito@gmail.com>
2014-07-11 14:54:03 -0700

    add exhibitors json

--- cull.js
+++ cull.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 
-// sessions.json is the response from 
+// sessions.json is the response from
 // http://cdh.esri.com/2.0/?action=getWebAppData&confid=65
 // it's ~3.5MB
 // after removing unused info, it's ~1MB, probably ~200K gzipped
@@ -19,11 +19,13 @@ fs.exists(out, function(exists) {
 
 function go() {
   fs.readFile(source, "utf-8", function(err, data) {
-    sessions(data);
+    cleanData = sanitize(data)
+    sessions(cleanData, "sessions-culled.json");
+    exhibitors(cleanData, "exhibitors-culled.json");
   });
 }
 
-function sessions(data) {
+function sanitize(data) {
   // 7/5/2014:  there were a few backslashes in weird places that were
   // causing errors when passing data with JSON.parse().
   // Examples:  www.fema.gov\hazus, GIS Analyst\Environmental Scientist
@@ -31,6 +33,45 @@ function sessions(data) {
   data = data.replace(/\\([^"])/g, function(m, g) { return "/" + g; });
   // Strip vertical tabs, they also break stuff.
   data = data.replace(/\v/g, "");
+  return data;
+}
+
+// NOTE: expects clean (read: legal) JSON data beyond here
+
+function exhibitors(data, output) {
+  var s = JSON.parse(data);
+  var results = s.exhibitorsView.results
+  var relevant = {}
+  relevant.conference = {
+    results: [{
+      fullname: s.conference.results[0].fullname,
+      year: s.conference.results[0].year
+    }]
+  };
+  relevant.exhibitorsView = {};
+  relevant.exhibitorsView.count = s.exhibitorsView.count;
+  relevant.exhibitorsView.results = results.map(function(exhibitor) {
+    return {
+      xPoint: exhibitor.xPoint,
+      yPoint: exhibitor.yPoint,
+      exhibitorBooth: exhibitor.exhibitorBooth,
+      exhibitorName: exhibitor.exhibitorName,
+      exhibitorDescription: exhibitor.exhibitorDescription,
+      exhibitorURL: exhibitor.exhibitorURL,
+      sponsorLogo: exhibitor.sponsorLogo,
+      sponsorTypeID: exhibitor.sponsorTypeID,
+      sponsorTypeName: exhibitor.sponsorTypeName
+    }
+  });
+  fs.writeFile(output, JSON.stringify(relevant), function(err) {
+    if ( err ) {
+      throw err;
+    }
+    console.log("Wrote file", output);
+  });
+}
+
+function sessions(data, output) {
   var s = JSON.parse(data);
   var results = s.sessionsView.results;
   var relevant = {};
@@ -63,10 +104,10 @@ function sessions(data) {
       paperTitle: session.paperTitle
     }
   });
-  fs.writeFile(out, JSON.stringify(relevant), function(err) {
+  fs.writeFile(output, JSON.stringify(relevant), function(err) {
     if ( err ) {
       throw err;
     }
-    console.log("Wrote file", out);
+    console.log("Wrote file", output);
   });
 }
